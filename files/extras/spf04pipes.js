046ddc6ea1ec5a3eec80db5e8350b6e27d7173f9
Christopher Collins <cacollins@cacollins-21616.local>
2017-09-18 14:15:17 -0400

    init

---
+++ files/extras/spf04pipes.js
@@ -0,0 +1,87 @@
+function initialize ()
+	{
+		dwg1=document.getElementById("dwg1");
+		dwg2=document.getElementById("dwg2");
+		dwg3=document.getElementById("dwg3");
+		dwg4=document.getElementById("dwg4");
+		dwg5=document.getElementById("dwg5");
+		dwg6=document.getElementById("dwg6");
+		dwg7=document.getElementById("dwg7");
+		dwg8=document.getElementById("dwg8");
+		dwg9=document.getElementById("dwg9");
+		dwg10=document.getElementById("dwg10");
+	}
+	
+function clearStage()
+	{
+      dwg1.style.display='none'
+      dwg2.style.display='none'
+      dwg3.style.display='none'
+      dwg4.style.display='none'
+      dwg5.style.display='none'
+      dwg6.style.display='none'
+      dwg7.style.display='none'
+      dwg8.style.display='none'
+      dwg9.style.display='none'
+      dwg10.style.display='none'
+	}
+		
+function displaydwg1()
+	{
+		clearStage()
+		dwg1.style.display='inline'
+    }
+
+function displaydwg2()
+	{	
+		clearStage()
+		dwg2.style.display='inline'
+    }
+
+function displaydwg3()
+	{	
+		clearStage()
+		dwg3.style.display='inline'
+    }
+
+function displaydwg4()
+	{	
+		clearStage()
+		dwg4.style.display='inline'
+    }
+
+function displaydwg5()
+	{	
+		clearStage()
+		dwg5.style.display='inline'
+    }
+
+function displaydwg6()
+	{	
+		clearStage()
+		dwg6.style.display='inline'
+    }
+
+function displaydwg7()
+	{	
+		clearStage()
+		dwg7.style.display='inline'
+    }
+
+function displaydwg8()
+	{	
+		clearStage()
+		dwg8.style.display='inline'
+    }
+    
+function displaydwg9()
+	{	
+		clearStage()
+		dwg9.style.display='inline'
+    }
+
+function displaydwg10()
+	{	
+		clearStage()
+		dwg10.style.display='inline'
+    }
